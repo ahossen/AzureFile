801145f59d1a1ad6bacf8524c2172bbc0c4af5d0
ahossen <azad0429@gmail.com>
2020-10-29 12:38:51 +0100

    first commit

---
+++ Resource added to Subs.ps1
@@ -0,0 +1,4 @@
+﻿Get-AzResourceProvider -ListAvailable
+Register-AzResourceProvider -ProviderNamespace Microsoft.Cdn
+((Get-AzResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).Locations
+((Get-AzResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions
